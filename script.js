df79776815b14a4277b613e1e1c9e9a4f86ea44e
Deep Chand <deepc2060@gmail.com>
2026-08-08 21:46:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function highlight() {
-    //Write your code here
+    const strongTags = document.querySelectorAll("strong");
 
-
+    strongTags.forEach((element) => {
+        element.style.color = "rgb(0, 128, 0)";
+    });
 }
 
-
 function return_normal() {
-    //Write your code here
+    const strongTags = document.querySelectorAll("strong");
 
-    
+    strongTags.forEach((element) => {
+        element.style.color = "rgb(0, 0, 0)";
+    });
 }
